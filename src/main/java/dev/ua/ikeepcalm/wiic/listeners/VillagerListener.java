ae6f9e25d596a4b577e1b1abce04025e3fd7d93a
Sóla Lusøt <60041069+solaluset@users.noreply.github.com>
2024-11-24 15:12:05 +0200

    Make villager trades conversions more flexible (#4)
    * Create separate config section for villagers
    
    * Add option to disable conversion of trade results
    
    * Add option to only use coins for rare items
    
    * Add option to update existing trades
    
    * Fix bug that made villagers accumulate outdated data
    
    * Ignore current recipe count when adding missing data

--- src/main/java/dev/ua/ikeepcalm/wiic/listeners/VillagerListener.java
+++ src/main/java/dev/ua/ikeepcalm/wiic/listeners/VillagerListener.java
@@ -3,59 +3,170 @@ package dev.ua.ikeepcalm.wiic.listeners;
 import dev.ua.ikeepcalm.wiic.WIIC;
 import dev.ua.ikeepcalm.wiic.utils.CoinUtil;
 import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.entity.AbstractVillager;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.VillagerAcquireTradeEvent;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.MerchantRecipe;
+import org.bukkit.persistence.PersistentDataContainer;
+import org.bukkit.persistence.PersistentDataType;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class VillagerListener implements Listener {
     private final WIIC plugin;
+    private final NamespacedKey originalCostsKey;
+    private final NamespacedKey originalResultsKey;
+    private final NamespacedKey villagerVersionKey;
 
     public VillagerListener(WIIC plugin) {
         this.plugin = plugin;
+        originalCostsKey = new NamespacedKey(plugin, "original_costs");
+        originalResultsKey = new NamespacedKey(plugin, "original_results");
+        villagerVersionKey = new NamespacedKey(plugin, "version");
     }
 
     @EventHandler
     public void onVillagerAcquireTrade(VillagerAcquireTradeEvent event) {
+        saveOriginalCosts(event.getEntity(), event.getRecipe());
         event.setRecipe(convertRecipe(event.getRecipe()));
     }
 
-    private MerchantRecipe convertRecipe(MerchantRecipe recipe) {
-        int emeralds = 0;
-        List<ItemStack> ingredients = recipe.getIngredients();
-        for (ItemStack item : new ArrayList<>(ingredients)) {
+    @EventHandler
+    public void onInteractEntity(PlayerInteractEntityEvent event) {
+        if (event.getRightClicked() instanceof AbstractVillager villager) {
+            int currentVersion = plugin.getConfig().getInt("villagers.version");
+            PersistentDataContainer pdc = villager.getPersistentDataContainer();
+            if (!Integer.valueOf(currentVersion).equals(
+                pdc.get(villagerVersionKey, PersistentDataType.INTEGER)
+            )) {
+                int[] originalCosts = pdc.get(originalCostsKey, PersistentDataType.INTEGER_ARRAY);
+                int[] originalResults = pdc.get(originalResultsKey, PersistentDataType.INTEGER_ARRAY);
+                if (originalCosts == null || originalResults == null) {
+                    return;
+                }
+
+                if (villager.getRecipeCount() > originalCosts.length) {
+                    for (int i = originalCosts.length; i < villager.getRecipeCount(); i++) {
+                        saveOriginalCosts(villager, villager.getRecipe(i), true);
+                    }
+                    originalCosts = pdc.get(originalCostsKey, PersistentDataType.INTEGER_ARRAY);
+                    originalResults = pdc.get(originalResultsKey, PersistentDataType.INTEGER_ARRAY);
+                }
+
+                for (int i = 0; i < villager.getRecipeCount(); i++) {
+                    MerchantRecipe recipe = villager.getRecipe(i);
+                    List<ItemStack> ingredients = recipe.getIngredients();
+                    ingredients.removeIf(ingredient -> ingredient.getType() == Material.EMERALD || CoinUtil.isCoin(ingredient));
+                    int emeralds = originalCosts[i];
+                    if (emeralds > 64) {
+                        emeralds -= 64;
+                        ingredients.add(new ItemStack(Material.EMERALD, 64));
+                    }
+                    if (emeralds > 0) {
+                        ingredients.add(new ItemStack(Material.EMERALD, emeralds));
+                    }
+                    ItemStack result = recipe.getResult();
+                    if (result.getType() == Material.EMERALD || CoinUtil.isCoin(result)) {
+                        result = new ItemStack(Material.EMERALD, originalResults[i]);
+                    }
+
+                    recipe = new MerchantRecipe(
+                        result,
+                        recipe.getUses(),
+                        recipe.getMaxUses(),
+                        recipe.hasExperienceReward(),
+                        recipe.getVillagerExperience(),
+                        recipe.getPriceMultiplier(),
+                        recipe.getDemand(),
+                        recipe.getSpecialPrice(),
+                        recipe.shouldIgnoreDiscounts()
+                    );
+                    recipe.setIngredients(ingredients);
+
+                    villager.setRecipe(i, convertRecipe(recipe));
+                }
+
+                pdc.set(villagerVersionKey, PersistentDataType.INTEGER, currentVersion);
+            }
+        }
+    }
+
+    private void saveOriginalCosts(AbstractVillager villager, MerchantRecipe recipe) {
+        saveOriginalCosts(villager, recipe, false);
+    }
+
+    private void saveOriginalCosts(AbstractVillager villager, MerchantRecipe recipe, boolean ignoreCurrentCount) {
+        PersistentDataContainer pdc = villager.getPersistentDataContainer();
+
+        if (!pdc.has(villagerVersionKey)) {
+            pdc.set(villagerVersionKey, PersistentDataType.INTEGER, plugin.getConfig().getInt("villagers.version"));
+        }
+
+        int[] costsArray = pdc.get(originalCostsKey, PersistentDataType.INTEGER_ARRAY);
+        int[] resultsArray = pdc.get(originalResultsKey, PersistentDataType.INTEGER_ARRAY);
+
+        if (costsArray != null && resultsArray != null && (villager.getRecipeCount() == costsArray.length || ignoreCurrentCount)) {
+            costsArray = Arrays.copyOf(costsArray, costsArray.length + 1);
+            resultsArray = Arrays.copyOf(resultsArray, resultsArray.length + 1);
+        } else {
+            costsArray = new int[1];
+            resultsArray = new int[1];
+        }
+
+        for (ItemStack item : recipe.getIngredients()) {
             if (item.getType() == Material.EMERALD) {
-                emeralds += item.getAmount();
-                ingredients.remove(item);
+                costsArray[costsArray.length - 1] += item.getAmount();
             }
         }
-
-        int coppets = emeraldsToCoppets(emeralds);
-        if (ingredients.isEmpty()) {
-            int licks = coppets / 64;
-            if (licks > 0) {
-                ingredients.add(CoinUtil.getLick(licks));
-            }
-
-            coppets = coppets % 64;
-            if (coppets > 0) {
-                ingredients.add(CoinUtil.getCoppet(coppets));
-            }
-        } else if (ingredients.size() == 1 && coppets > 0) {
-            if (coppets < 64) {
-                ingredients.addFirst(CoinUtil.getCoppet(coppets));
-            } else {
-                ingredients.addFirst(CoinUtil.getLick((int) Math.round(coppets / 64.0)));
-            }
+        if (recipe.getResult().getType() == Material.EMERALD) {
+            resultsArray[resultsArray.length - 1] = recipe.getResult().getAmount();
         }
 
+        pdc.set(originalCostsKey, PersistentDataType.INTEGER_ARRAY, costsArray);
+        pdc.set(originalResultsKey, PersistentDataType.INTEGER_ARRAY, resultsArray);
+    }
+
+    private MerchantRecipe convertRecipe(MerchantRecipe recipe) {
         ItemStack result = recipe.getResult();
-        if (result.getType() == Material.EMERALD) {
-            coppets = emeraldsToCoppets(result.getAmount());
+        List<ItemStack> ingredients = recipe.getIngredients();
+
+        if (!plugin.getConfig().getBoolean("villagers.useCoinsOnlyForRareItems") || isRare(result.getType())) {
+            int emeralds = 0;
+            for (ItemStack item : new ArrayList<>(ingredients)) {
+                if (item.getType() == Material.EMERALD) {
+                    emeralds += item.getAmount();
+                    ingredients.remove(item);
+                }
+            }
+
+            int coppets = emeraldsToCoppets(emeralds);
+            if (ingredients.isEmpty()) {
+                int licks = coppets / 64;
+                if (licks > 0) {
+                    ingredients.add(CoinUtil.getLick(licks));
+                }
+
+                coppets = coppets % 64;
+                if (coppets > 0) {
+                    ingredients.add(CoinUtil.getCoppet(coppets));
+                }
+            } else if (ingredients.size() == 1 && coppets > 0) {
+                if (coppets < 64) {
+                    ingredients.addFirst(CoinUtil.getCoppet(coppets));
+                } else {
+                    ingredients.addFirst(CoinUtil.getLick((int) Math.round(coppets / 64.0)));
+                }
+            }
+        }
+
+        if (plugin.getConfig().getBoolean("villagers.convertTradeResults") && result.getType() == Material.EMERALD) {
+            int coppets = emeraldsToCoppets(result.getAmount());
             if (coppets >= 64) {
                 result = CoinUtil.getLick((int) Math.round(coppets / 64.0));
             } else {
@@ -83,10 +194,14 @@ public class VillagerListener implements Listener {
         if (emeralds == 0) {
             return 0;
         }
-        int coppets = (int) Math.round(emeralds * plugin.getConfig().getDouble("emeraldsToCoppetsMultiplier"));
+        int coppets = (int) Math.round(emeralds * plugin.getConfig().getDouble("villagers.emeraldsToCoppetsMultiplier"));
         if (coppets == 0) {
             return 1;
         }
         return coppets;
+    }
+
+    private boolean isRare(Material material) {
+        return material == Material.ENCHANTED_BOOK || material.getMaxDurability() != 0;
     }
 }
