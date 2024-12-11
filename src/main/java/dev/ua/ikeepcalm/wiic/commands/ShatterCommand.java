f3e5d2d302e1d58dab39c26d38691d4b20be0871
Sóla Lusøt <60041069+solaluset@users.noreply.github.com>
2024-12-11 22:01:00 +0200

    Improve shatter command
    Move quantity check into one place

--- src/main/java/dev/ua/ikeepcalm/wiic/commands/ShatterCommand.java
+++ src/main/java/dev/ua/ikeepcalm/wiic/commands/ShatterCommand.java
@@ -19,21 +19,17 @@ public class ShatterCommand implements CommandExecutor {
         if (commandSender instanceof Player player && command.getName().equalsIgnoreCase("shatter")) {
             ItemStack item = player.getInventory().getItemInMainHand();
             if (CoinUtil.isCoin(item)) {
+                if (item.getAmount() > 1) {
+                    player.sendMessage(Component.text("Візьміть одну штуку у головну руку!").color(NamedTextColor.RED));
+                    return true;
+                }
                 switch (ItemUtil.getType(item)) {
                     case "verlDor" -> {
-                        if (item.getAmount() > 1) {
-                            player.sendMessage(Component.text("Візьміть одну штуку у головну руку!").color(NamedTextColor.RED));
-                            return true;
-                        }
                         player.getInventory().setItemInMainHand(new ItemStack(Material.AIR));
                         player.getInventory().addItem(CoinUtil.getLick(64));
                         player.sendMessage(Component.text("Предмет розбитий!").color(NamedTextColor.GREEN));
                     }
                     case "lick" -> {
-                        if (item.getAmount() > 1) {
-                            player.sendMessage(Component.text("Цей предмет не може бути розбитий!").color(NamedTextColor.RED));
-                            return true;
-                        }
                         player.getInventory().setItemInMainHand(new ItemStack(Material.AIR));
                         player.getInventory().addItem(CoinUtil.getCoppet(64));
                         player.sendMessage(Component.text("Предмет розбитий!").color(NamedTextColor.GREEN));
