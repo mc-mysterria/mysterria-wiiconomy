a39e3ba85edfc1d0ebe4e833991ec3928cc4403a
Sóla Lusøt <60041069+solaluset@users.noreply.github.com>
2025-04-04 19:37:50 +0300

    Add alternative coppet prices support (#16)

--- src/main/java/dev/ua/ikeepcalm/wiic/guis/ShopGUI.java
+++ src/main/java/dev/ua/ikeepcalm/wiic/guis/ShopGUI.java
@@ -3,16 +3,19 @@ package dev.ua.ikeepcalm.wiic.guis;
 import com.github.stefvanschie.inventoryframework.gui.GuiItem;
 import com.github.stefvanschie.inventoryframework.gui.type.ChestGui;
 import com.github.stefvanschie.inventoryframework.pane.OutlinePane;
+import dev.ua.ikeepcalm.market.util.AuctionUtil;
 import dev.ua.ikeepcalm.market.util.ItemStackUtil;
 import dev.ua.ikeepcalm.wiic.WIIC;
 import dev.ua.ikeepcalm.wiic.economy.Shop;
 import dev.ua.ikeepcalm.wiic.economy.ShopItem;
 import dev.ua.ikeepcalm.wiic.utils.Requester;
+import dev.ua.ikeepcalm.wiic.utils.VaultUtil;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.format.TextDecoration;
 import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
@@ -41,43 +44,85 @@ public class ShopGUI {
         }
     }
 
-    public void open() {
+    public void open(boolean useCoppets) {
         final ChestGui gui = new ChestGui(shop.getRows(), shop.getTitle());
         gui.setOnGlobalClick(event -> event.setCancelled(true));
 
+        int size = 9 * (shop.getRows() - 1);
         final OutlinePane pane = new OutlinePane(9, shop.getRows());
 
         for (ShopItem shopItem : shop.getItems()) {
+            if (useCoppets && shopItem.coppetPrice() == -1) continue;
+            size--;
             final ItemStack item = shopItem.item().clone();
             final ItemMeta meta = item.getItemMeta();
             meta.lore(List.of(
-                    Component.text("Ціна: " + shopItem.price() + " UNI").color(NamedTextColor.GREEN).decoration(TextDecoration.ITALIC, false),
+                    Component.text(
+                            useCoppets ? "Ціна: " + AuctionUtil.getFormattedPrice(shopItem.coppetPrice())
+                                    : "Ціна: " + shopItem.price() + " UNI").color(NamedTextColor.GREEN)
+                            .decoration(TextDecoration.ITALIC, false),
                     Component.text(shopItem.description()).decoration(TextDecoration.ITALIC, false)
             ));
             item.setItemMeta(meta);
             pane.addItem(new GuiItem(item, event -> Bukkit.getScheduler().runTaskAsynchronously(
                     plugin,
-                    () -> buyItem(shopItem.item().clone(), shopItem.price())
+                    () -> buyItem(shopItem.item().clone(), useCoppets ? shopItem.coppetPrice() : shopItem.price(), useCoppets)
             )));
+        }
+
+        for (int i = 0; i < size; i++) {
+            pane.addItem(new GuiItem(new ItemStack(Material.AIR)));
+        }
+        for (int i = 0; i < 4; i++) {
+            pane.addItem(new GuiItem(getFillerItem()));
+        }
+        pane.addItem(new GuiItem(getCurrencySwitcherItem(useCoppets), event -> open(!useCoppets)));
+        for (int i = 0; i < 4; i++) {
+            pane.addItem(new GuiItem(getFillerItem()));
         }
 
         gui.addPane(pane);
         gui.show(player);
     }
 
-    private void buyItem(ItemStack item, int price) {
+    private void buyItem(ItemStack item, int price, boolean useCoppets) {
         final String itemName = PlainTextComponentSerializer.plainText().serialize(item.displayName());
         plugin.getLogger().info(player.getName() + " has bought " + itemName + " for " + price);
-        try {
-            int userId = Requester.fetchUser(player.getName()).getId();
-            Requester.modifyBalance(userId, userId, -price, "Купівля предмета: " + itemName);
-        } catch (Exception e) {
-            e.printStackTrace();
-            player.sendMessage(Component.text("Під час придбання предмета відбулася помилка!").color(NamedTextColor.RED));
-            return;
+        if (!useCoppets) {
+            try {
+                int userId = Requester.fetchUser(player.getName()).getId();
+                Requester.modifyBalance(userId, userId, -price, "Купівля предмета: " + itemName);
+            } catch (Exception e) {
+                e.printStackTrace();
+                player.sendMessage(Component.text("Під час придбання предмета відбулася помилка!").color(NamedTextColor.RED));
+                return;
+            }
+        } else {
+            if (VaultUtil.getBalance(player.getUniqueId()).join() >= price) {
+                VaultUtil.withdraw(player.getUniqueId(), price);
+            } else {
+                player.sendMessage(Component.text("Недостатньо коштів в гаманці!").color(NamedTextColor.RED));
+                return;
+            }
         }
         Bukkit.getScheduler().runTask(plugin, () -> ItemStackUtil.giveOrDrop(player, item));
         player.sendMessage(Component.text("Ви успішно придбали предмет!").color(NamedTextColor.GREEN));
+    }
+
+    private ItemStack getFillerItem() {
+        final ItemStack item = new ItemStack(Material.WHITE_STAINED_GLASS_PANE);
+        final ItemMeta meta = item.getItemMeta();
+        meta.itemName(Component.empty());
+        item.setItemMeta(meta);
+        return item;
+    }
+
+    private ItemStack getCurrencySwitcherItem(boolean useCoppets) {
+        final ItemStack item = new ItemStack(useCoppets ? Material.GOLD_INGOT : Material.DIAMOND);
+        final ItemMeta meta = item.getItemMeta();
+        meta.itemName(Component.text("Валюта: " + (useCoppets ? "копійки" : "UNI")).color(NamedTextColor.GREEN));
+        item.setItemMeta(meta);
+        return item;
     }
 
     public static void clearShopCache() {
